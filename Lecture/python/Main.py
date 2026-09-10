0a907e561412a02586a3ae2197e94d9c179d93fd
yannawarchinmay0806-commits <yannawarchinmay0806@gmail.com>
2026-09-10 14:21:31 +0530

    .

---
+++ Lecture/python/Main.py
@@ -0,0 +1,39 @@
+
+#1. write a program to check if a number is odd or even 
+ #ex , 2 = no. is odd :false
+#num = int(input("Enter a number: "))
+#print("Is the number odd:", num % 2 != 0)
+
+#2. write a program to print age in days 
+ #ex, 3 years = 1095 days , print("Enter age")
+# Program to print age in days 
+#age = int(input("Enter age in years: "))
+#days = age * 365
+#print("Your age in days is:", days)
+
+#3. write a prograam to convert hour into min
+#ex, 135 is 2 hours 15 minutes
+# Program to print min in hour
+# Program to convert min into hour
+# Program to convert minutes into hours
+print("Enter Min")
+min = int(input())
+hour = min / 60
+print("Hour =", hour)
+
+#4. write a program to extract the last four digits of an number 
+# ex: 1234: last  digit is 4 
+# accpet the string or mode
+
+#5.write a program to check if a person is eligible for discunt the criteria is he must me a student and age must be below 21
+# input values to take are role and age 
+# Ex: Eligible : True . if the person is qualifying the criteria true otherwise flase   
+
+#role = input("Enter your role: ")
+#age = int(input("Enter your age: "))
+#eligible = (role.lower() == "student") and (age < 21)
+#print("Eligible:", eligible)
+
+#write a programm to swap two variables without a third variable, using arithmatic operations 
+#example : before swap a=10, b=20
+          # after swap =20 , b=10        
