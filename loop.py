55ed2b4afac08d79793f7e19850dab6ad1b72885
yannawarchinmay0806-commits <yannawarchinmay0806@gmail.com>
2026-09-15 14:06:54 +0530

    asdf

--- loop.py
+++ loop.py
@@ -1,6 +1,13 @@
 #loop 
+correct_pass="some_pass"
+not_found = True
 
+while not_found:
+    string = input("Enter a String: ")
+    if string == correct_pass: 
+      not_found = False 
+    else:
+     print("Wrong password try again!! ")
+     
+print("Password Matched! ")
 
-while True:
-    string = input("Enter a String:")
-    print(string)
