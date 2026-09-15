70b2dead18eb58b9e6d3dc9ecb1963132c7df806
yannawarchinmay0806-commits <yannawarchinmay0806@gmail.com>
2026-09-15 15:22:35 +0530

    version 1.6

---
+++ Average.py
@@ -0,0 +1,5 @@
+#enter a list of numbers which display sum and average of the numbers in the list
+numbers = [1, 2, 3, 4, 5]
+total = sum(numbers)
+average = total / len(numbers)
+print(f"Sum: {total}, Average: {average}")
---
+++ Break.py
@@ -0,0 +1,8 @@
+#using while and continue 
+i = 0
+while i < 10:
+    if i == 5:
+        continue
+
+    print(i)
+    i += 1
