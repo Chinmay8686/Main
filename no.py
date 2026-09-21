1f16f17a7e2d16f28b5bfee000d8a69ec14a8922
yannawarchinmay0806-commits <yannawarchinmay0806@gmail.com>
2026-09-21 11:09:02 +0530

    version 30

--- no.py
+++ no.py
@@ -6,7 +6,7 @@ def count_digits(number):
     while number > 0:
         number //= 10
         count += 1 
-    return count
+    return count 
 
 num = int(input("Enter a number: "))
 print("Number of digits:", count_digits(num))
